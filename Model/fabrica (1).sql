af45fe3e587f85af5d8ba27b6ab41f51c6d2e37d
Julio Cesar <julio66tt@gmail.com>
2026-04-08 21:55:00 -0300

    Add files via upload

---
+++ Model/fabrica (1).sql
@@ -0,0 +1,258 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 09/04/2026 às 02:00
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `fabrica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome_cliente` varchar(255) DEFAULT NULL,
+  `end_cliente` varchar(255) DEFAULT NULL,
+  `contato` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `entrada`
+--
+
+CREATE TABLE `entrada` (
+  `id_entrada` int(11) NOT NULL,
+  `data` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id_cliente` int(11) DEFAULT NULL,
+  `forma_pagamento` varchar(255) DEFAULT NULL,
+  `valor_entrada` decimal(10,2) DEFAULT NULL,
+  `peso_total` decimal(10,3) DEFAULT NULL,
+  `id_ped` int(11) DEFAULT NULL,
+  `tipo` enum('pedido','manual') NOT NULL DEFAULT 'pedido'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Acionadores `entrada`
+--
+DELIMITER $$
+CREATE TRIGGER `before_insert_entrada` BEFORE INSERT ON `entrada` FOR EACH ROW BEGIN
+
+  -- DECLARE sempre no topo
+  DECLARE v_id_cliente INT;
+  DECLARE v_valor DECIMAL(10,2);
+  DECLARE v_peso DECIMAL(10,3);
+
+  IF NEW.tipo = 'pedido' THEN
+
+    SELECT id_cliente
+    INTO v_id_cliente
+    FROM pedidos
+    WHERE id_ped = NEW.id_ped
+    LIMIT 1;
+
+    SELECT COALESCE(SUM(quantidade * valor_unitario),0)
+    INTO v_valor
+    FROM itens_pedido
+    WHERE id_ped = NEW.id_ped;
+
+    SELECT COALESCE(SUM(quantidade * peso_unid),0)
+    INTO v_peso
+    FROM itens_pedido
+    WHERE id_ped = NEW.id_ped;
+
+    SET NEW.id_cliente = v_id_cliente;
+    SET NEW.valor_entrada = v_valor;
+    SET NEW.peso_total = v_peso;
+
+  END IF;
+
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `itens_pedido`
+--
+
+CREATE TABLE `itens_pedido` (
+  `id_item` int(11) NOT NULL,
+  `id_ped` int(11) DEFAULT NULL,
+  `id_prod` int(11) DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `valor_unitario` decimal(10,2) DEFAULT NULL,
+  `peso_unid` decimal(10,3) DEFAULT NULL,
+  `total` decimal(10,2) GENERATED ALWAYS AS (`quantidade` * `valor_unitario`) VIRTUAL,
+  `peso_total` decimal(10,3) GENERATED ALWAYS AS (`quantidade` * `peso_unid`) VIRTUAL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id_ped` int(11) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `data` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id_prod` int(11) NOT NULL,
+  `spec_prod` varchar(255) DEFAULT NULL,
+  `peso_prod` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `cargo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Índices de tabela `entrada`
+--
+ALTER TABLE `entrada`
+  ADD PRIMARY KEY (`id_entrada`),
+  ADD KEY `fk_entrada_cliente` (`id_cliente`),
+  ADD KEY `fk_entrada_pedido` (`id_ped`);
+
+--
+-- Índices de tabela `itens_pedido`
+--
+ALTER TABLE `itens_pedido`
+  ADD PRIMARY KEY (`id_item`),
+  ADD KEY `id_ped` (`id_ped`),
+  ADD KEY `id_prod` (`id_prod`);
+
+--
+-- Índices de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id_ped`),
+  ADD KEY `fk_pedidos_cliente` (`id_cliente`);
+
+--
+-- Índices de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id_prod`);
+
+--
+-- Índices de tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de tabela `entrada`
+--
+ALTER TABLE `entrada`
+  MODIFY `id_entrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT de tabela `itens_pedido`
+--
+ALTER TABLE `itens_pedido`
+  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id_ped` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `entrada`
+--
+ALTER TABLE `entrada`
+  ADD CONSTRAINT `fk_entrada_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
+  ADD CONSTRAINT `fk_entrada_pedido` FOREIGN KEY (`id_ped`) REFERENCES `pedidos` (`id_ped`);
+
+--
+-- Restrições para tabelas `itens_pedido`
+--
+ALTER TABLE `itens_pedido`
+  ADD CONSTRAINT `itens_pedido_ibfk_1` FOREIGN KEY (`id_ped`) REFERENCES `pedidos` (`id_ped`),
+  ADD CONSTRAINT `itens_pedido_ibfk_2` FOREIGN KEY (`id_prod`) REFERENCES `produtos` (`id_prod`);
+
+--
+-- Restrições para tabelas `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `fk_pedidos_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
